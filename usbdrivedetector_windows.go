d8a53c4505760ec12904a7657faa5cf4d951fab6
Deepak Jois <deepak.jois@gmail.com>
2016-10-22 20:51:33 +0530

    Add untested support for Windows

---
+++ usbdrivedetector_windows.go
@@ -0,0 +1,61 @@
+package usbdrivedetector
+
+import (
+	"bufio"
+	"bytes"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+// Detect returns a list of file paths pointing to the root folder of
+// USB storage devices connected to the system.
+func Detect() ([]string, error) {
+	var drives []string
+	driveMap := make(map[string]bool)
+
+	cmd := "wmic"
+	args := []string{"logicaldisk", "where", "drivetype=2", "get", "deviceid"}
+	out, err := exec.Command(cmd, args...).Output()
+
+	if err != nil {
+		return drives, err
+	}
+
+	s := bufio.NewScanner(bytes.NewReader(out))
+	for s.Scan() {
+		line := s.Text()
+		if line != "" && line != "DeviceID" {
+			rootPath := line + string(os.PathSeparator)
+			driveMap[rootPath] = true
+		}
+	}
+
+	for k := range driveMap {
+		_, err := os.Open(k)
+		if err == nil {
+			drives = append(drives, k)
+		}
+	}
+
+	return drives, nil
+}
+
+func isUSBStorage(device string) bool {
+	deviceVerifier := "ID_USB_DRIVER=usb-storage"
+	cmd := "udevadm"
+	args := []string{"info", "-q", "property", "-n", device}
+	out, err := exec.Command(cmd, args...).Output()
+
+	if err != nil {
+		log.Printf("Error checking device %s: %s", device, err)
+		return false
+	}
+
+	if strings.Contains(string(out), deviceVerifier) {
+		return true
+	}
+
+	return false
+}
